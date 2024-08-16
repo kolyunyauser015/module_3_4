ba0713b8463058138f1e5153e24d026845e3ae75
kolyunyauser015 <89058507+kolyunyauser015@users.noreply.github.com>
2024-08-16 09:39:45 +0600

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,9 +1,10 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in other_words:
-        if root_word.lower() in i.lower():
+        if root_word.lower() in i.lower() or i.lower() in root_word.lower():
             same_words.append(i)
     return same_words
 
 
-print(single_root_words('работ', 'работа', 'среда', 'РАБОТАТЬ', 'прибрать', 'работник'))
+print(single_root_words('работ', 'работа', 'раб', 'среда', 'РАБОТАТЬ', 'прибрать', 'работник'))
+
